487cb380eaf9f495a4f539498d08db344fdb6a10
arcsin16 <arcsin16@gmail.com>
2017-08-11 11:44:30 +0900

    Initial commit

---
+++ Assets/GestureInputTest/Scripts/IPInputBehaviour.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using UnityEngine;
+
+namespace arcsin16.GestureInputTest
+{
+    public class IPInputBehaviour : MonoBehaviour
+    {
+        public TextMesh IpText;
+        public AudioClip upSound;
+        public AudioClip downSound;
+        public AudioClip rightSound;
+        public AudioClip leftSound;
+        private string ipText;
+        private static readonly string PROMPT = "SharingServer IP:\r\n";
+        private static readonly Dictionary<string, KeyCode> GesturePatternDic = new Dictionary<string, KeyCode>()
+    {
+        {"UL", KeyCode.Alpha1 },
+        {"U",  KeyCode.Alpha2 },
+        {"UR", KeyCode.Alpha3 },
+        {"LU", KeyCode.Alpha4 },
+        {"L",  KeyCode.Alpha5 },
+        {"LD", KeyCode.Alpha6 },
+        {"RU", KeyCode.Alpha7 },
+        {"R",  KeyCode.Alpha8 },
+        {"RD", KeyCode.Alpha9 },
+        {"DL", KeyCode.Delete },
+        {"D",  KeyCode.Alpha0 },
+        {"DR", KeyCode.Period },
+    };
+
+        private Dictionary<UDRLGestureDetector.Direction, Func<AudioClip>> AudioClipDic;
+
+        void OnEnable()
+        {
+            UDRLGestureDetector.GestureDetected += GestureDetected;
+        }
+
+        void OnDisable()
+        {
+            UDRLGestureDetector.GestureDetected -= GestureDetected;
+        }
+
+        void Start()
+        {
+            this.AudioClipDic = new Dictionary<UDRLGestureDetector.Direction, Func<AudioClip>>()
+            {
+                { UDRLGestureDetector.Direction.Up,    ()=> this.upSound},
+                { UDRLGestureDetector.Direction.Down,  ()=> this.downSound},
+                { UDRLGestureDetector.Direction.Right, ()=> this.rightSound},
+                { UDRLGestureDetector.Direction.Left,  ()=> this.leftSound},
+            };
+
+            this.ipText = string.Empty;
+        }
+
+        void GestureDetected(object sender, UDRLGestureDetector.GestureEventArgs args)
+        {
+            // ジェスチャー中：現在選択中の入力を表示する
+            if (args.Type == UDRLGestureDetector.GestureEventType.DETECTING)
+            {
+                KeyCode keyCode = GetKeyCode(args);
+                // 選択中の表示を更新
+                if (!IsValidInput(this.ipText, keyCode))
+                {
+                    IpText.text = PROMPT + this.ipText + "[]";
+                }
+                else
+                {
+                    if (keyCode == KeyCode.Delete)
+                    {
+                        IpText.text = PROMPT + this.ipText + "[DEL]";
+                    }
+                    else
+                    {
+                        IpText.text = PROMPT + this.ipText + "[" + (char)keyCode + "]";
+                    }
+                }
+
+                // TODO: 現在認識しているジェスチャーを視覚的に表示する
+
+                // ジェスチャーが認識されたタイミングを、効果音で通知する
+                AudioClip clip = GetAudioClipOrNull(args);
+                if (clip != null)
+                    AudioSource.PlayClipAtPoint(clip, this.transform.position, 0.8f);
+
+            }
+
+            // ジェスチャー確定：入力を確定する
+            else if (args.Type == UDRLGestureDetector.GestureEventType.DETECTED)
+            {
+                KeyCode keyCode = GetKeyCode(args);
+                if (!IsValidInput(this.ipText, keyCode))
+                {
+                    IpText.text = PROMPT + this.ipText;
+                    return;
+                }
+                // 削除処理
+                if (keyCode == KeyCode.Delete)
+                {
+                    // 直前の文字が.の場合２文字消す
+                    if (this.ipText.EndsWith(".") && this.ipText.Length > 1)
+                    {
+                        this.ipText = this.ipText.Remove(this.ipText.Length - 2);
+                    }
+                    // 通常は１文字
+                    else if (this.ipText.Length > 0)
+                    {
+                        this.ipText = this.ipText.Remove(this.ipText.Length - 1);
+                    }
+                }
+                else if (keyCode == KeyCode.Period)
+                {
+                    var blocks = this.ipText.Split('.');
+                    if (blocks.Length == 4)
+                    {
+                        // IP 入力完了
+                        this.InputCompleted();
+                    }
+                    else
+                    {
+                        this.ipText += '.';
+                    }
+                }
+                // 文字入力
+                else
+                {
+                    this.ipText += (char)keyCode;
+
+                    var blocks = this.ipText.Split('.');
+                    var editingBlock = blocks.LastOrDefault() ?? string.Empty;
+                    // . を自動保管
+                    if (editingBlock.Length == 3)
+                    {
+                        if (blocks.Length != 4)
+                        {
+                            this.ipText += ".";
+                        }
+                        else
+                        {
+                            this.InputCompleted();
+                        }
+                    }
+                }
+
+                IpText.text = PROMPT + this.ipText;
+            }
+        }
+
+        private void InputCompleted()
+        {
+            // TODO: SharingStageのServeAddressを設定して、サーバに接続する
+            if (this.upSound != null)
+                AudioSource.PlayClipAtPoint(this.upSound, this.transform.position, 0.8f);
+            if (this.rightSound != null)
+                AudioSource.PlayClipAtPoint(this.rightSound, this.transform.position, 0.8f);
+            if (this.leftSound != null)
+                AudioSource.PlayClipAtPoint(this.leftSound, this.transform.position, 0.8f);
+            if (this.downSound != null)
+                AudioSource.PlayClipAtPoint(this.downSound, this.transform.position, 0.8f);
+        }
+
+        /// <summary>
+        /// 入力コードのバリデーション
+        /// </summary>
+        /// <param name="ip"></param>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        private bool IsValidInput(string ip, KeyCode input)
+        {
+            if (input == KeyCode.None)
+                return false;
+
+            var editingBlock = ip.Split('.').LastOrDefault() ?? string.Empty;
+
+            // 空白時のDelete
+            if (input == KeyCode.Delete && ip.Length == 0)
+                return false;
+
+            // . の連続
+            if (input == KeyCode.Period && editingBlock.Length == 0)
+                return false;
+
+
+            // 数値入力
+            if (input != KeyCode.Period && input != KeyCode.Delete)
+            {
+                // 0～255 の範囲外
+                Debug.LogFormat("Input {0}", (char)input);
+                int val = int.Parse(editingBlock + (char)input);
+                if (val < 0 || val > 255)
+                    return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// ジェスチャーに対応するKeyCodeを取得する
+        /// 対応するものがなければKeyCode.None
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        private static KeyCode GetKeyCode(UDRLGestureDetector.GestureEventArgs args)
+        {
+            KeyCode value;
+            return GesturePatternDic.TryGetValue(args.Pattern, out value) ? value : KeyCode.None;
+        }
+
+        /// <summary>
+        /// ジェスチャーに対応するAudioClipを取得する
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        private AudioClip GetAudioClipOrNull(UDRLGestureDetector.GestureEventArgs args)
+        {
+            Func<AudioClip> clipFunc;
+            return AudioClipDic.TryGetValue(args.Direction, out clipFunc) ? clipFunc() : null;
+        }
+    }
+}
---
+++ Assets/GestureInputTest/Scripts/UDRLGestureDetector.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.VR.WSA.Input;
+
+namespace arcsin16.GestureInputTest
+{
+    public class UDRLGestureDetector : MonoBehaviour
+    {
+
+        public TextMesh GestureDebugText;
+        public enum Direction
+        {
+            Neutral, Up, Down, Right, Left
+        }
+
+        public enum GestureEventType
+        {
+            DETECTING, DETECTED
+        }
+
+        private static readonly Dictionary<Direction, char> DirectionCodeDic = new Dictionary<Direction, char>()
+        {
+            {Direction.Up,    'U' },
+            {Direction.Down,  'D' },
+            {Direction.Right, 'R' },
+            {Direction.Left,  'L' },
+        };
+
+        public class GestureEventArgs : EventArgs
+        {
+            public GestureEventType Type { get; set; }
+            public Direction Direction { get; set; }
+            public string Pattern { get; set; }
+        }
+
+        public static EventHandler<GestureEventArgs> GestureDetected;
+
+        public float GestureThreshold = 0.05f;
+
+        private bool notifyGesture;
+
+        private Vector3 lastPos;
+        private Direction currentDirection;
+        private Vector3 axisY;
+        private Vector3 axisX;
+
+        private StringBuilder gesturePattern = new StringBuilder();
+        private float accumulatedDx;
+        private float accumulatedDy;
+
+        void OnEnable()
+        {
+            InteractionManager.SourcePressed += OnSourcePressed;
+            InteractionManager.SourceReleased += OnSourceReleased;
+            InteractionManager.SourceUpdated += OnSourceUpdated;
+            InteractionManager.SourceLost += OnSourceLost;
+            InteractionManager.SourceDetected += OnSourceDetected;
+        }
+
+
+
+        void OnDisable()
+        {
+            InteractionManager.SourcePressed -= OnSourcePressed;
+            InteractionManager.SourceReleased -= OnSourceReleased;
+            InteractionManager.SourceUpdated -= OnSourceUpdated;
+            InteractionManager.SourceLost -= OnSourceLost;
+            InteractionManager.SourceDetected -= OnSourceDetected;
+        }
+
+        private void OnSourceDetected(InteractionSourceState state)
+        {
+            Vector3 pos;
+            if (state.properties.location.TryGetPosition(out pos))
+            {
+                this.lastPos = pos;
+            }
+
+            this.notifyGesture = false;
+            this.currentDirection = Direction.Neutral;
+            this.accumulatedDx = 0;
+            this.accumulatedDy = 0;
+        }
+
+        private void OnSourcePressed(InteractionSourceState state)
+        {
+            // タップ開始時にジェスチャー通知を有効化
+            // ※タップ開始前から、ジェスチャーの追跡は行っている。
+            if (!this.notifyGesture)
+            {
+                this.notifyGesture = true;
+                gesturePattern.Remove(0, gesturePattern.Length);
+
+                // タップし始めたタイミングで、事前の移動量を多少引き継ぐ
+                int signX = this.accumulatedDx < 0 ? -1 : 1;
+                int signY = this.accumulatedDy < 0 ? -1 : 1;
+                this.accumulatedDx = Mathf.Min(this.accumulatedDx, GestureThreshold);
+                this.accumulatedDy = Mathf.Min(this.accumulatedDy, GestureThreshold);
+                this.accumulatedDx = Mathf.Max(this.accumulatedDx, -GestureThreshold);
+                this.accumulatedDy = Mathf.Max(this.accumulatedDy, -GestureThreshold);
+                this.accumulatedDx /= 2;
+                this.accumulatedDy /= 2;
+            }
+        }
+
+        private void OnSourceUpdated(InteractionSourceState state)
+        {
+            // ジェスチャーを更新
+            TraceGesture(state);
+        }
+
+        private void OnSourceReleased(InteractionSourceState state)
+        {
+            // タップ終了時にジェスチャーを確定
+            NotifyGestureDetected();
+        }
+
+        private void OnSourceLost(InteractionSourceState state)
+        {
+            // トラッキングロスト時にジェスチャーを確定
+            NotifyGestureDetected();
+        }
+
+        private void TraceGesture(InteractionSourceState state)
+        {
+            // カメラの上方向をy軸、右方向をx軸とする
+            this.axisY = Camera.main.transform.up;
+            this.axisX = Camera.main.transform.right;
+
+            // 手の位置を取得
+            Vector3 pos;
+            if (state.properties.location.TryGetPosition(out pos))
+            {
+                // 手の移動量
+                var diff = pos - this.lastPos;
+                this.lastPos = pos;
+
+                // x,y軸方向の移動量を取得する
+                float dx = Vector3.Dot(this.axisX, diff);
+                float dy = Vector3.Dot(this.axisY, diff);
+
+                // 誤差の蓄積で暴発しないように、dx, dyの大きい要素のみ加算する
+                // TODO: これむしろダメかも
+                if (Mathf.Abs(dx) > Mathf.Abs(dy))
+                {
+                    // 検知中の方向への加算は0に抑制する
+                    if (this.currentDirection == Direction.Right && dx > 0)
+                    {
+                        this.accumulatedDx += 0;
+                    }
+                    else if (this.currentDirection == Direction.Left && dx < 0)
+                    {
+                        this.accumulatedDx += 0;
+                    }
+                    else
+                    {
+                        this.accumulatedDx += dx;
+                    }
+                }
+                else
+                {
+                    // 検知中の方向への加算は0に抑制する
+                    if (this.currentDirection == Direction.Up && dy > 0)
+                    {
+                        this.accumulatedDy += 0;
+                    }
+                    else if (this.currentDirection == Direction.Down && dy < 0)
+                    {
+                        this.accumulatedDy += 0;
+                    }
+                    else
+                    {
+                        this.accumulatedDy += dy;
+                    }
+                }
+
+                // 蓄積した移動量が閾値以上になれば、ジェスチャーと判定
+                // Right
+                Direction dir = Direction.Neutral;
+                if (this.accumulatedDx > GestureThreshold)
+                {
+                    dir = Direction.Right;
+                }
+                // Left
+                else if (this.accumulatedDx < -GestureThreshold)
+                {
+                    dir = Direction.Left;
+                }
+                // Up
+                else if (this.accumulatedDy > GestureThreshold)
+                {
+                    dir = Direction.Up;
+                }
+                // Down
+                else if (this.accumulatedDy < -GestureThreshold)
+                {
+                    dir = Direction.Down;
+                }
+
+                if (dir != Direction.Neutral && this.currentDirection != dir)
+                {
+                    Debug.LogFormat("Gesture Detect {0}", dir);
+                    if (this.GestureDebugText != null) this.GestureDebugText.text += string.Format("\r\n Detect Gesture: {0}", dir);
+                    this.currentDirection = dir;
+                    this.accumulatedDx = 0;
+                    this.accumulatedDy = 0;
+
+                    this.gesturePattern.Append(DirectionCodeDic[dir]);
+                    if (this.notifyGesture)
+                    {
+                        GestureDetected(this, new GestureEventArgs()
+                        {
+                            Type = GestureEventType.DETECTING,
+                            Direction = this.currentDirection,
+                            Pattern = this.gesturePattern.ToString()
+                        });
+                    }
+                }
+            }
+            else
+            {
+                Debug.Log("InteractionSourceLocation::TryGetPosition failed.");
+                if (this.GestureDebugText != null) this.GestureDebugText.text += "\r\n InteractionSourceLocation::TryGetPosition failed";
+            }
+        }
+
+        private void NotifyGestureDetected()
+        {
+            if (this.notifyGesture)
+            {
+                if (this.currentDirection != Direction.Neutral)
+                {
+                    GestureDetected(this, new GestureEventArgs()
+                    {
+                        Type = GestureEventType.DETECTED,
+                        Direction = this.currentDirection,
+                        Pattern = this.gesturePattern.ToString()
+                    });
+                }
+
+                // Clear
+                this.notifyGesture = false;
+                this.currentDirection = Direction.Neutral;
+                this.accumulatedDx = 0;
+                this.accumulatedDy = 0;
+            }
+        }
+    }
+}
